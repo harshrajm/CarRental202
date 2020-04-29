525169b30b32cea8ae6a7023044c15a402219ded
thepotla <36852419+thepotla@users.noreply.github.com>
2020-04-29 11:38:28 -0700

    manage users update

--- car_rental_fe/src/components/manageUsers.jsx
+++ car_rental_fe/src/components/manageUsers.jsx
@@ -1,15 +1,61 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { getUserList } from "../services/backendCallService";
+import axios from 'axios';
 
-class ManageUsers extends Component {
-  state = {};
+export default class ManageUsers extends Component {
+  state = { user: [] };
+  
+  async componentDidMount() {
+    const { data: user } = await getUserList();
+    this.setState({ user });
+
+    this.deleteUser = this.deleteUser.bind(this)
+  }
+
+  deleteUser(id) {
+    axios.delete('http://localhost:8080/admin/user'+id)
+      .then(response => { console.log(response.data)});
+
+    this.setState({
+      item: this.state.user.filter(el => el._id !== id)
+    })
+  }
+
+  getUsersList() {
+    if (this.state.user) {
+      return this.state.user.map(item => (
+        <tr>
+          <td>{item.username}</td>
+          <td>{item.name}</td>
+          <td>{item.email}</td>
+          <td>{item.membershipActive.toString()}</td>
+          <td>
+      <a href="#" onClick={() => { item.deleteUser(item._id) }}>delete</a>
+    </td>
+        </tr>
+      ));
+    }
+  }
+  
   render() {
     return (
-      <React.Fragment>
-        <h2>Manage Users</h2>
-        <hr />
-      </React.Fragment>
-    );
+      <div>
+        <h3>Manage Users</h3>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th>Username</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Membership Active</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.getUsersList()}
+          </tbody>
+        </table>
+      </div>
+    )
   }
 }
 
-export default ManageUsers;
